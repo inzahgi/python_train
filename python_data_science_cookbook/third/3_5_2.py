2d9e6b4503dd4b6be195a3d01280d06a0b2f0d89
inzahgi <inzahgi@126.com>
2018-03-26 00:58:47 +0800

    fix ch3

--- python_data_science_cookbook/third/3_5_2.py
+++ python_data_science_cookbook/third/3_5_2.py
@@ -14,7 +14,7 @@ y_labels = range(1, 26)
 
 plt.close('all')
 
-plt.figure(1)
+##plt.figure(1)
 fig,ax = plt.subplots()
 ax.pcolor(x_, cmap=plt.cm.Greens, edgecolors='k')
 ax.set_xticks(np.arange(0, x_.shape[1]) + 0.5)
@@ -26,7 +26,31 @@ ax.set_yticklabels(y_labels, minor=False, fontsize=10)
 
 
 ################################
+x1 = x[0:50]
+x2 = x[50:99]
+x3 = x[100:149]
 
+x1 = scale(x1, with_std=False)
+x2 = scale(x2, with_std=False)
+x3 = scale(x3, with_std=False)
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+y_labels = range(1, 51)
+
+ax1.set_xticks(np.arange(0, x.shape[1]) + 0.5)
+ax1.set_yticks(np.arange(0, 50, 10))
+
+ax1.xaxis.tick_bottom()
+ax1.set_xticklabels(col_names, minor=False, fontsize=2)
+
+ax1.pcolor(x1, cmap=plt.cm.Greens, edgecolors='k')
+ax1.set_title(data['target_names'][0])
+
+ax2.pcolor(x2, cmap=plt.cm.Greens, edgecolors='k')
+ax2.set_title(data['target_names'][1])
+
+ax3.pcolor(x1, cmap=plt.cm.Greens, edgecolors='k')
+ax3.set_title(data['target_names'][2])
 
 ###################################
 plt.show()
